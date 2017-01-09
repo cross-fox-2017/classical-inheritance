4ef96bf152996a3d88ffc44563ea6cdd71765836
Syanmil <syanmil@yahoo.com>
2017-01-09 11:51:19 +0700

    superpowerd frog get electrocute then can use laser

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,15 +1,16 @@
 "use strict"
 
 class Animal{
-  constructor(num_legs, is_warm_blooded){
+  constructor(nama, num_legs, is_warm_blooded){
     this.num_legs = num_legs;
+    this.nama = nama
     this.is_warm_blooded = is_warm_blooded
   }
 }
 
 class Mammal extends Animal{
-  constructor(num_legs, is_warm_blooded){
-    super(num_legs, is_warm_blooded)
+  constructor(nama, num_legs, is_warm_blooded){
+    super(nama, num_legs, is_warm_blooded)
     this.num_legs = 4;
     this.is_warm_blooded = true;
     this.is_has_egg = false
@@ -17,28 +18,42 @@ class Mammal extends Animal{
 }
 
 class Primate extends Mammal{
-  constructor(num_legs, is_warm_blooded){
-    super(num_legs, is_warm_blooded)
+  constructor(nama, num_legs, is_warm_blooded){
+    super(nama, num_legs, is_warm_blooded)
     this.num_legs = 2;
     this.hairy = true;
   }
 }
 
 class Chimpz extends Primate{
-  constructor(num_legs, is_warm_blooded){
-    super(num_legs, is_warm_blooded)
+  constructor(nama, num_legs, is_warm_blooded){
+    super(nama, num_legs, is_warm_blooded)
   }
 }
 
 class Reptile extends Animal{
-  constructor(num_legs, is_warm_blooded){
-    super(num_legs, is_warm_blooded)
+  constructor(nama, num_legs, is_warm_blooded){
+    super(nama, num_legs, is_warm_blooded)
     this.num_legs = 4;
     this.is_warm_blooded = false
   }
 }
 
-var bobo = new Chimpz()
-var kadal = new Reptile()
+class SuperPoweredFrog extends Reptile{
+  constructor(nama, num_legs, is_warm_blooded){
+    super(nama, num_legs, is_warm_blooded)
+    this.useLaser = false
+  }
+  electrocute(){
+    this.useLaser = true
+    return this
+  }
+}
+
+var bobo = new Chimpz("hutan")
+var kadal = new Reptile("Liza")
+var frogMan = new SuperPoweredFrog("CrazyFoo")
 console.log(bobo);
 console.log(kadal);
+console.log(frogMan);
+console.log(frogMan.electrocute());
