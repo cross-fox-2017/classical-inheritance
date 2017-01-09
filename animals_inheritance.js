be8d5a0afe4fc7030af652ae88c68fb578f731a7
Mangku Widodo <mangkuwi26@gmail.com>
2017-01-09 11:32:38 +0700

    inheritance done, method superpowerd menyusul

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,1 +1,25 @@
 "use strict"
+class Animal {
+  constructor(name) {
+    this._name = name
+  }
+  myNameIs(){
+    console.log(`The Species Name is: ${this._name}`);
+  }
+  superPowered(){
+    
+  }
+}
+
+class Mammal extends Animal {
+  constructor(name) {
+    super(name)
+  }
+  myNameIs(){
+    super.myNameIs()
+  }
+}
+
+let hewan = new Mammal('kuda')
+hewan.myNameIs()
+// console.log(hewan.myNameIs());
