3537067ccbf6bfc903f993a966050296dfd2d932
Samue PH Sinaga <samuelphsinaga@gmail.com>
2017-01-09 17:57:24 +0700

    done

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -6,21 +6,30 @@ class Animal {
 }
 
 class Amphibian extends Animal{
-  constructor (nama, is_warm_blooded){
-    super (nama)
+  constructor (jenis, is_warm_blooded){
+    super (jenis)
     this.is_warm_blooded = is_warm_blooded
   }
 }
 
 class Frog extends  Amphibian{
-  constructor(nama, is_warm_blooded, warna){
-    super (nama, is_warm_blooded)
+  constructor(jenis, is_warm_blooded, warna){
+    super (jenis, is_warm_blooded)
     this.warna = warna
-  }
-  print(){
-    return `jenis : ${this.jenis}, Berdarah Hangat : ${this.is_warm_blooded}, Warna: ${this.warna}`
   }
 }
 
-var xxx = new Frog('Amphibian', 'Tidak','Hijau')
-console.log(xxx.print())
+class SuperPowerFrog extends Frog{
+  constructor(jenis, is_warm_blooded, warna, kekuatan){
+  super (jenis, is_warm_blooded, warna)
+  this.kekuatan = kekuatan
+  }
+  power(){
+    this.kekuatan = "Berenang Cepat"
+    return `jenis : ${this.jenis}, Berdarah Hangat : ${this.is_warm_blooded}, Warna: ${this.warna}, kekuatan: ${this.kekuatan}`
+  }
+}
+
+
+var xxx = new SuperPowerFrog('Amphibian', 'Tidak','Hijau')
+console.log(xxx.power())
