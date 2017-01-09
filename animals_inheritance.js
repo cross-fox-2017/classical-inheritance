039d1ef744a1e7fdd2b18d50fe785a24e9b1f48c
Mangku Widodo <mangkuwi26@gmail.com>
2017-01-09 16:02:55 +0700

    update superpower class, and finish

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,25 +1,47 @@
 "use strict"
 class Animal {
-  constructor(name) {
+  constructor(name, leg) {
     this._name = name
+    this._leg = leg
   }
   myNameIs(){
-    console.log(`The Species Name is: ${this._name}`);
-  }
-  superPowered(){
-    
+    console.log(`The Species Name is: ${this._name}, Animal Leg : ${this._leg}`);
   }
 }
 
 class Mammal extends Animal {
-  constructor(name) {
-    super(name)
+  constructor(name, leg) {
+    super(name, leg)
+  }
+  myNameIs(){
+    super.myNameIs()
+  }
+
+}
+
+class Primate extends Animal {
+  constructor(name, leg) {
+    super(name, leg)
   }
   myNameIs(){
     super.myNameIs()
   }
 }
 
-let hewan = new Mammal('kuda')
-hewan.myNameIs()
+class SuperPoweredHorse extends Mammal{
+  constructor(name, leg) {
+    super(name, leg)
+    this._ability =
+  }
+  be_invisible(){
+    console.log(`This ${this._name} has Invisible Ability`);
+  }
+}
+
+// let hewan = new Mammal()
+let horse = new SuperPoweredHorse('kuda','4')
+// hewan.myNameIs()
+horse.myNameIs()
+horse.be_invisible()
+// hewan.be_invisible()
 // console.log(hewan.myNameIs());
