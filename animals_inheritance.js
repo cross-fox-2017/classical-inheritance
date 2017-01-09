b751fc5f2be62f4c81b9ab6e17a1fd8d3b36d7ea
Samue PH Sinaga <samuelphsinaga@gmail.com>
2017-01-09 16:04:43 +0700

    oke

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,1 +1,26 @@
 "use strict"
+class Animal {
+  constructor (jenis){
+    this.jenis = jenis
+  }
+}
+
+class Amphibian extends Animal{
+  constructor (nama, is_warm_blooded){
+    super (nama)
+    this.is_warm_blooded = is_warm_blooded
+  }
+}
+
+class Frog extends  Amphibian{
+  constructor(nama, is_warm_blooded, warna){
+    super (nama, is_warm_blooded)
+    this.warna = warna
+  }
+  print(){
+    return `jenis : ${this.jenis}, Berdarah Hangat : ${this.is_warm_blooded}, Warna: ${this.warna}`
+  }
+}
+
+var xxx = new Frog('Amphibian', 'Tidak','Hijau')
+console.log(xxx.print())
