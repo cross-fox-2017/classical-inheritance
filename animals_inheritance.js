88f8c4a60745a82af6fcc213c90338a5171f307f
Irsan Sebastian <email": null>
2017-01-09 16:09:26 +0700

    first commit

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,1 +1,68 @@
 "use strict"
+
+class Animal{
+
+      constructor(numleg){
+            this._numleg = numleg || 0;
+            this._is_warm_blooded = "true";
+      }
+
+
+}
+
+class Mammal extends Animal{
+
+        constructor(numleg){
+
+            super(numleg);
+        }
+
+
+}
+
+class Primate extends Animal{
+
+        constructor(numleg){
+
+            super(numleg);
+        }
+
+}
+
+
+class Chimpanzee extends Primate{
+
+      constructor(numleg,name){
+          super(numleg);
+          this._name = name;
+      }
+
+      getName(){
+        return this._name;
+      }
+
+}
+
+class superPoweredChim extends Chimpanzee{
+
+        constructor(numleg,name){
+            super(numleg,name)
+        }
+
+        joke(){
+
+            return "Irsan Handsome";
+
+        }
+
+}
+
+var irsan = new Chimpanzee(2,"irsan");
+var iqbal = new Primate(2);
+
+var skill = new superPoweredChim(2,"irsan");
+
+
+console.log(`jumlah kaki iqbal ${iqbal._numleg}`);
+console.log(`Jumlah kaki ${irsan.getName()} : ${irsan._numleg} \n`);
+console.log(`Say something funny : ${skill.joke()}`);
