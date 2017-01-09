0ee62523a16c632328afa948a4158a54022bd31a
unknown <yaumashf@gmail.com>
2017-01-09 11:32:59 +0700

    inherrit

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,1 +1,31 @@
 "use strict"
+class Animal {
+  constructor(blood, spesies, numLeg) {
+    this.spesies = spesies
+    this.blood = blood
+    this.numLeg = numLeg
+  }
+  animalBlood(){
+    console.log(`tipe darah: ${this.blood}`)
+  }
+  animalLeg(){
+    console.log(`jumlah kaki: ${this.leg}`)
+  }
+}
+
+class Aves extends Animal {
+  constructor(blood) {
+    super(blood)
+  }
+  animalBlood(){
+    super.animalBlood()
+  }
+}
+class Reptil extends Animal {
+  constructor() {
+
+  }
+}
+
+let binatang = new Aves('panas')
+ binatang.animalBlood()
