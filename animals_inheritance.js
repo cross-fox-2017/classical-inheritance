8323076319d02f021ea9bbab6370cb1072e38282
Ida Bagus Chahya Dhegana <dhegana@gmail.com>
2017-01-09 12:07:58 +0700

    dgana done all release

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,38 +1,40 @@
 "use strict"
 
-// class Animal{
-//   constructor(type, name) {
-//     this.type = type;
-//     this.name = name;
-//   }
-//   getData() {
-//     return console.log(`Animal Type: ${this.type}\nAnimal Name: ${this.name}`)
-//   }
-// }
-//
-// class Mammal extends Animal {
-//   constructor(type, name) {
-//     super(type, name)
-//   }
-//   getData() {
-//     super.getData();
-//   }
-// }
-//
-// class Chimpanzee extends Mammal {
-//   constructor(type, name, num_legs) {
-//     super(type, name)
-//     this.legs = num_legs;
-//   }
-//   getData() {
-//     super.getData();
-//     return console.log(`Animal Legs: ${this.legs}`)
-//   }
-// }
-//
-// let animal = new Chimpanzee('Mammal','Chimpanzee',2);
-// animal.getData();
+// RELEASE 0
+class Animal{
+  constructor(type, name) {
+    this.type = type;
+    this.name = name;
+  }
+  getData() {
+    return console.log(`Animal Type: ${this.type}\nAnimal Name: ${this.name}`)
+  }
+}
 
+class Mammal extends Animal {
+  constructor(type, name) {
+    super(type, name)
+  }
+  getData() {
+    super.getData();
+  }
+}
+
+class Chimpanzee extends Mammal {
+  constructor(type, name, num_legs) {
+    super(type, name)
+    this.legs = num_legs;
+  }
+  getData() {
+    super.getData();
+    return console.log(`Animal Legs: ${this.legs}`)
+  }
+}
+
+let animal = new Chimpanzee('Mammal','Chimpanzee',2);
+animal.getData();
+
+// RELEASE 1
 class SuperPowered {
   constructor(nama, jenis) {
     this.nama = nama;
